121494842b31e6f4cb43258acece5aa1c7913a87
Lukas Lysychas <sayshi108@gmail.com>
2020-04-08 03:00:22 +0300

    creating server

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("This is working!");
+});
+
+app.listen(3000, () => {
+  console.log("App is running on port 3000!");
+});
+
+/*
+/ --> res = this isworking
+/signin  --> POST = success/fail
+/register --> POST = user
+/profile/:userId --> GET = user
+/iamge --> PUT --> user
+
+
+*/
