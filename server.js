ad12737bb2ac37af8eef7be6f6fdb3b39e7530bb
Lukas Lysychas <sayshi108@gmail.com>
2020-04-17 14:58:42 +0300

    adding heroku postgres host

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const image = require("./controllers/image");
 const db = knex({
   client: "pg",
   connection: {
-    host: "127.0.0.1", //localhost
+    host: "postgresql-encircled-12325", //localhost
     user: "postgres",
     password: "test",
     database: "facebox",
